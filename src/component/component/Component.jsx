d1dc9494cf17ae45d275adc1234b2c9bf78d2c80
ccfine <469781947@qq.com>
2018-08-21 15:04:58 +0800

    增加视图组件

---
+++ src/component/component/Component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import styles from "./component.css"
+
+export default class Component extends Component {
+  render () {
+    return (
+      <div className={ styles.component }>
+        视图组件，与redux无关，可复用
+      </div>
+    )
+  }   
+}
