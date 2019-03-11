48e1c77a13a7a58aead584c8a36e49691c7d65a3
ccfine <469781947@qq.com>
2019-03-11 14:10:38 +0800

    生产环境关闭redux的控制台调试功能

--- src/index.js
+++ src/index.js
@@ -8,10 +8,16 @@ import App from "./App.jsx"
 import reducer from "./redux/reducer.js"
 import "css/global.css"
 
-const store = createStore(reducer, compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension? window.devToolsExtension(): f => f
-))
+let store
+
+if (process.env.NODE_ENV === "development") {
+  store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension? window.devToolsExtension(): f => f
+  ))
+} else if (process.env.NODE_ENV === "production") {
+  store = createStore(reducer, applyMiddleware(thunk))
+}
 
 ReactDOM.render(
   <Provider store={ store }>
