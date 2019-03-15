d82b041979fe49803e3a0de1b9f36e3f1017a16e
ccfine <469781947@qq.com>
2019-03-15 16:23:35 +0800

    修改关于redux控制台调试的配置

--- src/index.js
+++ src/index.js
@@ -11,10 +11,8 @@ import "css/global.css"
 let store
 
 if (process.env.NODE_ENV === "development") {
-  store = createStore(reducer, compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension? window.devToolsExtension(): f => f
-  ))
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 } else if (process.env.NODE_ENV === "production") {
   store = createStore(reducer, applyMiddleware(thunk))
 }
